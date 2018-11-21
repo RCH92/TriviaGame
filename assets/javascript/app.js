4287463bed77c06c1526ecd8066d55aac3a8274e
Hallenbeck <RCHallen92@gmail.com>
2018-11-20 23:09:13 -0700

    initial upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var questionBank = [
+    questionOne = [
+        question = "Who was the first man in Space?",
+        answer = "Neil Armstrong"
+    ]
+]
+
+
+function startScreen(){
+    
+    var start = $('<div>');
+
+    var button = $("<button>");
+    button.attr('id', "startButton");
+
+}
